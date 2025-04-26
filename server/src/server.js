4219b2dc774d10c2f54c4afd9ab3852feefd566d
mdsamialsohan <samialsohan@gmail.com>
2025-04-26 01:50:26 +0100

    morgan setup for developement purpose as i can see which api are called from browser

--- server/src/server.js
+++ server/src/server.js
@@ -1,9 +1,16 @@
 const express = require("express");
+const morgan = require('morgan');
 const app = express();
 
+app.use(morgan('dev'));
 app.get('/', (req, res)=>{
-    res.send('Welcome to employee management');
+    res.send('Welcome to employee management apps');
 })
+app.get('/test', (req,res)=>{
+res.status(200).send({
+    message: 'api testing is working fine',
+});
+});
 app.listen(3001,()=>{
     console.log(`server is running http://localhost:3001`)
 });
