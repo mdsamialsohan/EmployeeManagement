42e2dfea837fe1f2df671e85a616bd4b8f019708
mdsamialsohan <samialsohan@gmail.com>
2025-04-26 01:24:30 +0100

    new directory created for backend and front end

--- server/src/server.js
+++ server/src/server.js
@@ -1,5 +1,9 @@
 const express = require("express");
 const app = express();
+
+app.get('/', (req, res)=>{
+    res.send('Welcome to employee management');
+})
 app.listen(3001,()=>{
     console.log(`server is running http://localhost:3001`)
 });
